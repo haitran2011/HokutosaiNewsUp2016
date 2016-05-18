6ec829ba5be63bfdf73c939aa1961e9a67259b25
sejour <shuka.takakuma@gmail.com>
2016-05-18 16:12:24 +0900

    Initial Commit

---
+++ HokutosaiNewsUp2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HokutosaiNewsUp2016
+//
+//  Created by Shuka Takakuma on 2016/05/18.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
